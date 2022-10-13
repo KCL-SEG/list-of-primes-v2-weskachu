c1271303526d715d776086248e6b6e91575f98e4
Wesley Teo <ling.r.teo@kcl.ac.uk>
2022-10-13 17:02:18 +0100

    First attempt

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+    totalPrimes = 0
+    number = 3
+    isPrime = True
+
+    if number_of_primes <= 0:
+        raise ValueError(f"x = {x} should have been a positive number.")
+
+    list.append(2)
+
+    while totalPrimes < number_of_primes - 1:
+        for i in range(2, number):
+            if number % i == 0:
+                isPrime = False
+
+        if(isPrime == True):
+            list.append(number)
+            totalPrimes+= 1
+
+        isPrime = True
+        number += 1
+
     return list
